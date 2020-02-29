bd1e470a0f7dae9ae8f5154559287cfd32f6549c
HeeToTheHo <jaredwsokol@gmail.com>
2020-02-29 06:25:27 +0000

    added userID auth to reload command

--- commands/reload.js
+++ commands/reload.js
@@ -6,20 +6,24 @@ module.exports = {
 		const commandName = args[0].toLowerCase();
 		const command = msg.client.commands.get(commandName)
 			|| msg.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+		if (msg.author.id == '193454575624126464') {
+			if (!command) {
+				return msg.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+			}
 
-		if (!command) {
-			return msg.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
+			delete require.cache[require.resolve(`./${commandName}.js`)];
+
+			try {
+				const newCommand = require(`./${commandName}.js`);
+				msg.client.commands.set(newCommand.name, newCommand);
+			} catch (error) {
+				console.log(error);
+				return msg.channel.send(`There was an error while reloading a command \`${commandName}\`:\n\`${error.message}\``);
+			}
+			msg.channel.send(`Command \`${commandName}\` was reloaded!`);
 		}
-
-		delete require.cache[require.resolve(`./${commandName}.js`)];
-
-		try {
-			const newCommand = require(`./${commandName}.js`);
-			msg.client.commands.set(newCommand.name, newCommand);
-		} catch (error) {
-			console.log(error);
-			return msg.channel.send(`There was an error while reloading a command \`${commandName}\`:\n\`${error.message}\``);
+		else {
+			msg.channel.send(`You are not authorized to run that command!`)
 		}
-		msg.channel.send(`Command \`${commandName}\` was reloaded!`);
 	},
 };
